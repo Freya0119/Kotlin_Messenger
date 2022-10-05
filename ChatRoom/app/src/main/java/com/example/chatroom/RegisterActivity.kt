5f7b7971c415297a3cd65fa1c4de99d14cc9429c
unknown <103048480+Freya0119@users.noreply.github.com>
2022-10-05 15:33:19 +0800

    Kotlin_Messenger
    review
    
    What is the best to register?

--- ChatRoom/app/src/main/java/com/example/chatroom/RegisterActivity.kt
+++ ChatRoom/app/src/main/java/com/example/chatroom/RegisterActivity.kt
@@ -11,6 +11,7 @@ import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.auth.User
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_register.*
 import java.util.*
@@ -25,22 +26,27 @@ class RegisterActivity : AppCompatActivity() {
         }
         //登陸button
         bt_login.setOnClickListener() {
-            //change tag
-            Log.d("login", "try to login new activity")
-
-            val intent = Intent(this, LoginActivity::class.java)
+            val intent = Intent(this, LatestMessageActivity::class.java)
             startActivity(intent)
-            //TODO email password
-            FirebaseAuth.getInstance().signInWithEmailAndPassword("test@gmail.com", "123123")
+
+            //通過firebase驗證，所以不需要自己驗證email and password
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                edit_text_username.text.toString(),
+                edit_text_password.text.toString()
+            ).addOnSuccessListener {
+                Log.d("LOGIN", "Login Successful, try to into latest message.")
+            }.addOnFailureListener {
+                Log.d("LOGIN", "Login failed.")
+            }
         }
         //選擇圖片作為照片button
         bt_selected_photo.setOnClickListener() {
-            //change tag
-            Log.d("select", "choose one picture")
-
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
+            Log.d("IMAGE", "Click button for choose a picture.")
             //???
+            val intent = Intent(Intent.ACTION_PICK)
+            //選取image位置?
+            intent.type = "image/*"
+            //後面的onActivityResult
             startActivityForResult(intent, 0)
         }
     }
@@ -50,40 +56,49 @@ class RegisterActivity : AppCompatActivity() {
         //email password字串
         val strUsername = edit_text_username.text.toString()
         val strPassword = edit_text_password.text.toString()
-        //為空
+
+        //如果其中一個為空
         if ((strUsername.isEmpty()) || (strPassword.isEmpty())) {
-            Toast.makeText(this, "enter something", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "It is empty. Enter something.", Toast.LENGTH_SHORT).show()
             return
         }
-        //change tag
-        Log.d("show username", strUsername)
-        Log.d("show password", strPassword)
+
+        Log.d("SHOW", "Usr name is ${strUsername}, try to register or login.")
+        Log.d("SHOW", "Password is ${strPassword}, try to register or login.")
+
         //發送註冊
+        //這裡已經註冊用戶 firebaseAuth有data 所以後面可以取得uid
         FirebaseAuth.getInstance()
             .createUserWithEmailAndPassword(strUsername, strPassword)
             .addOnCompleteListener {
+                //return@addOnCompleteListener?
                 if (!it.isSuccessful) return@addOnCompleteListener
-                Log.d("register", "successful, uid: ${it.result.user?.uid}")
-
+                Log.d(
+                    "REGISTER",
+                    "Register successful, uid: ${it.result.user?.uid}. Try to upload image to Firebase."
+                )
+                //儲存到firebase
                 uploadImageToFirebaseStorage()
             }
             .addOnFailureListener {
-                Log.d("fail", "failed message: ${it.message}")
-                Toast.makeText(this, "enter something", Toast.LENGTH_SHORT).show()
+                Log.d("REGISTER", "Register failed, message: ${it.message}.")
             }
     }
 
-    //被選擇的圖片?
+    //被設置的圖片，因為會被改變所以放在外面?
     var selectedPhotoUri: Uri? = null
 
+    //從選擇照片返回register介面?onActivityResult有獲取返回值的功能
+    //和bt_select_image的startActivityForResult回應
+    //requestCode-> 功能Int
+    //resultCode-> RESULT-OK
+    //data-> 傳回來的data，例如:getExtra(USERNAME_KEY, username),getExtra(PASSWORD_KEY,password)，須為intent
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
+        //選擇成功
         if (requestCode == 0 && data != null && resultCode == Activity.RESULT_OK) {
-            //change tag
-            Log.d("selected", "photo can be choose")
+            Log.d("IMAGE", "Select photo successful.")
         }
-
         selectedPhotoUri = data?.data
 
         //設置photo和邊框
@@ -96,42 +111,43 @@ class RegisterActivity : AppCompatActivity() {
     //upload照片
     private fun uploadImageToFirebaseStorage() {
         if (selectedPhotoUri == null) return
-
-        val fileName = UUID.randomUUID().toString()
-        val ref = FirebaseStorage.getInstance().getReference("image/${fileName}")
-
+        //save with uid
+        val uid = FirebaseAuth.getInstance().uid
+        //這裡是儲存到storage 所以位置和user message不一樣
+        val ref = FirebaseStorage.getInstance().getReference("image/${uid}")
+        //upload image
         ref.putFile(selectedPhotoUri!!).addOnSuccessListener {
-            //change tag
-            Log.d("register", "upload photo to: ${it.metadata?.path}")
+            Log.d("IMAGE", "Upload image to: ${it.metadata?.path}.")
         }
-        //download link
+        //???downloadUrl onActivityResult的時候改變了image
+        //儲存以後打開上傳到firebase 為了把image path傳給saveUserToFirebaseDatabase 再存入user/
         ref.downloadUrl.addOnSuccessListener {
-            //change tag
-            Log.d("register", "photo url: ${it.toString()}")
-
+            Log.d(
+                "IMAGE", "Image url: ${it.toString()} successful. Try to save user to Firebase."
+            )
+            //save user to firebaseDatabase with imageUrl
+            //TODO move to bt_login?
             saveUserToFirebaseDatabase(it.toString())
         }.addOnFailureListener {
-            //TODO ?
+            Log.d("IMAGE", "Image url: ${it.toString()} failed.")
         }
     }
+
     //儲存到firebase
     private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
         val uid = FirebaseAuth.getInstance().uid ?: ""
-        val ref = FirebaseDatabase.getInstance().getReference("/user/${uid}")
+
+        val ref = FirebaseDatabase.getInstance().getReference("user/${uid}")
 
         val user = User(uid, edit_text_username.text.toString(), profileImageUrl)
 
         ref.setValue(user).addOnSuccessListener {
-            //change tag
-            Log.d("upload", "up photo success")
-
+            Log.d("SAVE USER TO FIREBASE", "Save user successful. Try into latest message.")
+            //註冊完成並打開latest message
             val intent = Intent(this, LatestMessageActivity::class.java)
+            //清除所有TASK並且把latestMessage_activity設為新的TASK?
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
         }
     }
-}
-
-class User(val uid: String, val username: String, val profileImageUrl: String) {
-    constructor() : this("", "", "")
 }
